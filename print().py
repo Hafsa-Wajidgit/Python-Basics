601f183336f82667748eb060da312c1193a07b10
Hafsa-Wajidgit <hafsawajidq22@gmail.com>
2024-12-13 23:11:46 +0500

    Update print().py

--- print().py
+++ print().py
@@ -1,8 +1,12 @@
 # A simple print function
 print("This is the first task of python!")
-# String manipulation. How to print double qoutes using single qoutes
-print('He said, "Wait for me!"')
+# How to print double qoutes using single qoutes
+print('He said,"Wait for me!"')
 # Single qoutes inside a double qoutes
 print("She said,'How are you doing?' with a big smile.")
-# \"escape\" teh code
+# using \"escape\" in the code
 print("Franz Kafka portrays true human nature in his novella known as \"Metamorphosis\".")
+# Creating new line with \n
+print("My Soldiers Rage! \nMy Soldiers Scream! \nMy Soldiers Fight!")
+# String concatenation using + sign
+print("Shinzo " + "wo " + "Sasageyo")
