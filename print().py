05ed6f6fe1dce2b6e1d37eb982d541d3c2f639fb
Hafsa-Wajidgit <hafsawajidq22@gmail.com>
2024-12-13 22:05:01 +0500

    Add files via upload
    This code display how to use print function in Python and how to print strings according to your desired outcome.

---
+++ print().py
@@ -0,0 +1,8 @@
+# A simple print function
+print("This is the first task of python!")
+# String manipulation. How to print double qoutes using single qoutes
+print('He said, "Wait for me!"')
+# Single qoutes inside a double qoutes
+print("She said,'How are you doing?' with a big smile.")
+# \"escape\" teh code
+print("Franz Kafka portrays true human nature in his novella known as \"Metamorphosis\".")
