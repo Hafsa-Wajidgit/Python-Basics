261b7c8748b2c7019500bed48f60aa644bfbb20c
Hafsa-Wajidgit <hafsawajidq22@gmail.com>
2024-12-14 00:09:44 +0500

    Add files via upload
    This code uses the input( ) function to get the input from the user then uses nested functions to print the input and also uses len( ) function to calculate the length of the input.

---
+++ input( ).py
@@ -0,0 +1,7 @@
+# intro to input() function.
+# input() gets input from the user.
+input("How old are you? ")
+# To display the input from the user, we use nested functions.
+print("Welcome " + input("What is your name? ") + "!")
+# we can calculate the length by using len().
+print(len(input("What is your favourite novel? ")))
