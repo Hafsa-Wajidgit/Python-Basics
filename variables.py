e7e13a67b740d0000ba591ebe18f698c1d43316e
Hafsa-Wajidgit <hafsawajidq22@gmail.com>
2024-12-14 21:26:43 +0500

    Add files via upload
    This code is an intro to the variables in Python. Variables are used to store data in the memory.

---
+++ variables.py
@@ -0,0 +1,8 @@
+# intro to variables.
+name = input("What is your name? ")
+# displaying variable
+print("Welcome " + name + "!")
+# we can calculate the length by using len().
+book = input("What's the name of your favourite novel? ")
+length = len(book)
+print(length)
