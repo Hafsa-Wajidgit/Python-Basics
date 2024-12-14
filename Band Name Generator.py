9a4d2b9dfc231a33e20c80699a994e4fd0c94c71
Hafsa-Wajidgit <hafsawajidq22@gmail.com>
2024-12-14 21:37:22 +0500

    Add files via upload
    A python band name generator beginner project uses all the concepts of print( ), input( ), variables , string manipulation etc.

---
+++ Band Name Generator.py
@@ -0,0 +1,8 @@
+# Greetings to the user
+print("Welcome to the Band Name Generator!")
+# Prompting for the input
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+# printing the result
+print("The name of your Band would be " + city + " " + pet)
+
